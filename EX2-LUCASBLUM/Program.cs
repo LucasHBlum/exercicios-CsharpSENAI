d786eb91dfdb483da4e26f47cb33439024616ac6
Lucas Henrique Blum <lucashblum@gmail.com>
2024-07-24 21:28:37 -0300

    Add files via upload

---
+++ EX2-LUCASBLUM/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    Console.WriteLine("Digite o numerador da primeira fração: ");
+        int numerador1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o denominador da primeira fração: ");
+        int denominador1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o numerador da segunda fração: ");
+        int numerador2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o denominador da segunda fração: ");
+        int denominador2 = int.Parse(Console.ReadLine());
+
+    int produtoNumerador = numerador1 * numerador2;
+    int produtoDenominador = denominador1 = denominador2;
+
+    int somaNumerador = numerador1 * denominador2 + numerador2 * denominador1;
+    int somaDenominador = denominador1 * denominador2;
+
+    Simplificar(ref somaNumerador, ref somaDenominador);
+    Simplificar(ref produtoNumerador, ref produtoDenominador);
+
+        Console.WriteLine($"Produto: {produtoNumerador}/{produtoDenominador}");
+        Console.WriteLine($"Soma: {somaNumerador}/{somaDenominador}");
+
+}      
+
+static void Simplificar(ref int numerador, ref int denominador)
+{
+    int gcd = MCD(numerador, denominador);
+    numerador /= gcd;
+    denominador /= gcd;
+}
+
+static int MCD(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+}
---
+++ EX3-LUCASBLUM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double raio, altura, volume;
+        const double PI = 3.14159;
+
+        Console.WriteLine("Digite o raio da base da lata de óleo (em cm): ");
+        raio = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a altura da lata de óleo (em cm): ");
+        altura = double.Parse(Console.ReadLine());
+
+        volume = PI * Math.Pow(raio, 2) * altura;
+
+        Console.WriteLine($"O volume da lata de óleo é: {volume:F2} cm³");
+
+    }
+}
