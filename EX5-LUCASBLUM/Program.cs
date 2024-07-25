7340c806d6935b86ea92dc2160741385919d5ece
Lucas Henrique Blum <lucashblum@gmail.com>
2024-07-24 21:57:11 -0300

    Calculadora

---
+++ EX5-LUCASBLUM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double numero1, numero2, soma, subtracao, multiplicacao, divisao;
+
+        Console.WriteLine("Digite o primeiro número: ");
+        numero1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        numero2 = double.Parse(Console.ReadLine());
+
+        soma = numero1 + numero2;
+        subtracao = numero1 - numero2;
+        multiplicacao = numero1 * numero2;
+        divisao = numero1 / numero2;
+
+        Console.WriteLine($"A soma dos números é: {soma:F2}");
+        Console.WriteLine($"A subtração dos números é: {subtracao:F2}");
+        Console.WriteLine($"A multiplicação dos números é: {multiplicacao:F2}");
+        Console.WriteLine($"A divisão dos números é: {divisao:F2}");
+    }
+}
