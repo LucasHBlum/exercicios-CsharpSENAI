9e326fb6f193b242a5a2a43be159122698d63a36
Lucas Henrique Blum <lucashblum@gmail.com>
2024-07-24 21:39:57 -0300

    Add files via upload

---
+++ EX4-LUCASBLUM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double nota1, nota2, peso1, peso2, mediaPonderada;
+
+        Console.WriteLine("Digite a nota da primeira prova: ");
+        nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o peso da primeira prova: ");
+        peso1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota da segunda prova: ");
+        nota2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o peso da segunda prova: ");
+        peso2 = double.Parse(Console.ReadLine());
+
+        mediaPonderada = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2);
+
+        Console.WriteLine($"A média ponderada das provas é: {mediaPonderada:F2}");
+            }
+}
