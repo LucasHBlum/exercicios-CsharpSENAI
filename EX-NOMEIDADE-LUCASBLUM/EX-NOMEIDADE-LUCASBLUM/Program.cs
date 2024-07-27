245ce82b3feab733918afcb4bffbf19ac3a70805
Lucas Henrique Blum <lucashblum@gmail.com>
2024-07-26 21:23:24 -0300

    Exercício Nome e Idade

---
+++ EX-NOMEIDADE-LUCASBLUM/EX-NOMEIDADE-LUCASBLUM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Digite seu nome completo: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Nome: {nome} \nIdade: {idade}");
+    }
+}
